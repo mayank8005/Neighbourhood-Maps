44f3aafd5dcf4cf049ec55effc65e7da6d825a74
Mayank Agrawal <mayank8005@gmail.com>
2017-10-26 21:32:05 +0530

    make map responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var map;
+
 $(document).ready(function () {
     
     //setting up toggle button
@@ -8,8 +10,13 @@ $(document).ready(function () {
         $('#option-panel').toggle();
         $('#toggle-open-area').toggle();
         $('#map').css('left', 0);
-
+        $('#map').css('width', '100%');
+        if($(window).width() <= 500){
+            $('#map').show();
+        }
+        resizeMap();
     });
+
 
     // open sidebar
     $('#toggle-open').click(function () {
@@ -17,16 +24,45 @@ $(document).ready(function () {
         $('#option-panel').toggle();
         $('#toggle-open-area').toggle();
         $('#map').css('left', 380);
+        $('#map').css('width', 'calc(100% - 380px)');
+        if($(window).width() <= 500){
+            $('#map').hide();
+        }
+        resizeMap();
+    });
+
+    $(window).on('resize', function(){
+        if($(window).width() > 500){
+            $('#map').show();
+        }
     });
 
 });
 
 function initMap() {
 
-    var map = new google.maps.Map($('#map')[0], {
+    map = new google.maps.Map($('#map')[0], {
         // initializing map data
         center: initialMapCenter,
         zoom: 13,
         mapTypeControl: false
     });
+
+    // setting event listener for resizing map
+    $(window).on('resize', function () {
+        resizeMap();
+    });
+
+
+}
+
+// function which resize maps
+function resizeMap(){
+    try{var currCenter = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(currCenter);
+    }
+    catch(err){
+        console.log('unable to resize map' + err);
+    }
 }
