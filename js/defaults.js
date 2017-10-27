9c7410a8bf71ab9c413f9a14e9b3dfb2f1f1e414
Mayank Agrawal <mayank8005@gmail.com>
2017-10-28 05:12:00 +0530

    added 1 location

--- js/defaults.js
+++ js/defaults.js
@@ -7,7 +7,8 @@ var initialLocation = [{title: 'DB city', location: {lat: 23.232618, lng: 77.429
                         {title: 'Lower Lake Bhopal', location: {lat: 23.242806, lng: 77.381067}, id: 2 },
                         {title: 'Ashima mall', location: {lat: 23.182838, lng: 77.456225}, id: 3 },
                         {title: 'Pizza hut', location: {lat: 23.236018, lng: 77.400446}, id: 4 },
-                        {title: 'Dominos', location: {lat: 23.239053, lng: 77.404722}, id: 5 }];
+                        {title: 'Dominos', location: {lat: 23.239053, lng: 77.404722}, id: 5 },
+                        {title: 'Shaurya Smarak', location: {lat: 23.2340790, lng: 77.4278990}, id: 6 }];
 
 
 // map style for google maps
